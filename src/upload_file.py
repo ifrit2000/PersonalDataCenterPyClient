2b8d5ba2f399cbbf8e750c15fd596aed0ecfd8bc
Anthony <cdistc@163.com>
2017-12-05 17:41:39 +0800

    first commit

--- src/upload_file.py
+++ src/upload_file.py
@@ -7,5 +7,5 @@ import urllib3
 file_id = "63UBnNc1Q8J18BDcsrbOQg=="
 
 http_pool = urllib3.PoolManager()
-response = http_pool.request("DELETE", "http://localhost:18888/file/test1/" + file_id)
+response = http_pool.request("DELETE", "http://localhost:18888/file/test66/" + file_id)
 print(json.loads(response.data))
